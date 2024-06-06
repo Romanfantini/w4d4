4a288a5e98791dd476f0a36a33eaa9d24dab3146
Romanfantini <fantiniroman5@gmail.com>
2024-06-06 21:17:56 +0200

    validate

--- java.js
+++ java.js
@@ -1,43 +1,47 @@
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll(".btn");
-
-
-
+const operator = ["+", "-", "*", "/"];
 let currentInput = "";
 
-
-function appendValue(number){
-    currentInput+= number;
-    display.innerText = currentInput;
+function appendValue(number) {
+  currentInput += number;
+  display.innerText = currentInput;
 }
 
-function clearValue(){
-    currentInput= "";
-    display.innerText = currentInput;
+function clearValue() {
+  currentInput = "";
+  display.innerText = currentInput;
 }
 
-function calculate(){
-    currentInput= eval(currentInput).toString();
+function calculate() {
+  const isValid = validate();
+  if (!isValid) {
+    currentInput = eval(currentInput).toString();
     display.innerText = currentInput;
+  }
 }
 
-buttons.forEach(button => {
-    button.addEventListener("click", () =>{
-        const value = button.innerText;
-        
-        switch(value) {
-            case "C":
-                clearValue(value);
-                break;
-            case "=":
-                calculate(value);
-                break;
-            default:
-                appendValue(value);
-                break;
-        }
-    })
+function validate() {
+  for (let i = 0; i < operator.length; i++) {
+    if (currentInput.startsWith(operator[i])) currentInput = "";
+    display.innerText = "ERROR";
+  }
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const value = button.innerText;
+
+    switch (value) {
+      case "C":
+        clearValue(value);
+        break;
+      case "=":
+        calculate(value);
+        break;
+      default:
+        appendValue(value);
+        break;
+    }
+  });
 });
-
-
-
